415e75d5f4add0477b2bdada03cd2ac9106edc18
Nihal Shukla <nshukla@supplyvalid.com>
2024-11-26 11:22:06 +0530

    Initial commit for Angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './mycomponents/home/home.component';
+import { UserComponent } from './mycomponents/user/user.component';
+import { AboutComponent } from './mycomponents/about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent,UserComponent,AboutComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
